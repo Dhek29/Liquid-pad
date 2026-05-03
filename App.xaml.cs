aabbb2067b10de603a6cf02c28a5181f0c8af43c
Dhek29 <dhek2006@gmail.com>
2026-05-03 15:52:45 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,16 +4,5 @@ namespace UsersFile
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            
-            DispatcherUnhandledException += (s, args) =>
-            {
-                MessageBox.Show($"Error: {args.Exception.Message}", "Error", 
-                              MessageBoxButton.OK, MessageBoxImage.Warning);
-                args.Handled = true;
-            };
-        }
     }
 }
